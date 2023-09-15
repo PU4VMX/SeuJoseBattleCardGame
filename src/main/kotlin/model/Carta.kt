65695bd1a1a3c0136377e2051b6902b935053aff
Vinicius Marques <viniciusmrqs@outlook.com>
2023-09-14 21:20:23 -0300

    Inicialização da classe carta e correção de erro que impedia a exibição das cartas lidas.

--- src/main/kotlin/model/Carta.kt
+++ src/main/kotlin/model/Carta.kt
@@ -1,5 +1,20 @@
 package model
 
 class Carta(s: String) {
+    val nome: String
+    val tipo: String
+    val ataque: Int
+    val defesa: Int
 
+    init {
+        val campos = s.split(";")
+        nome = campos[0]
+        tipo = String.format("%-10s", campos[1])
+        ataque = campos[2].toInt()
+        defesa = campos[3].toInt()
+    }
+
+    override fun toString(): String {
+        return "$nome - $tipo - $ataque - $defesa"
+    }
 }
